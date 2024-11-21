1a4042245aa412e5e87720fa421846ad43f27896
ketanvariya86 <ketanvariya86@gmail.com>
2024-11-21 19:50:28 +0530

    TC-03 : After login with Admin, verify all labels shown on Dashboard

--- src/test/java/com/ikkon/Test_cases/TC_SignInPageTest.java
+++ src/test/java/com/ikkon/Test_cases/TC_SignInPageTest.java
@@ -2,27 +2,16 @@ package com.ikkon.Test_cases;
 
 import static org.testng.Assert.assertEquals;
 
-
-import java.time.Duration;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-//import org.testng.annotations.BeforeTest;
-//import org.testng.annotations.Test;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.ikkon.pageObjects.DashboardPage;
 import com.ikkon.pageObjects.HomePage;
 import com.ikkon.pageObjects.SignInPage;
 
-import dev.failsafe.internal.util.Assert;
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class TC_SignInPageTest extends BaseClass {
 
-	@Test (priority = 1)
+	// TC-01 : Verify that Sign In page is opened or not
+	@Test(priority = 1)	
 	public void openSignInPage() throws InterruptedException {
 
 		HomePage hpage = new HomePage(driver); 
@@ -35,7 +24,10 @@ public class TC_SignInPageTest extends BaseClass {
 		assertEquals("Sign in to IKKON", pagename);
 		logger.info("SignIn page verified");
 	}
-	@Test (priority = 2)
+	
+	
+	// TC-02 : Verify that Admin is able to sign in successfully with correct credentials	
+	@Test (priority = 2)	
 	public void signInWithAdmin()throws InterruptedException{
 		HomePage hpage = new HomePage(driver);
 		hpage.ClickOnLogin();
@@ -56,5 +48,84 @@ public class TC_SignInPageTest extends BaseClass {
 		logger.info("Admin User Logged In successfully");
 
 	}
-
+	
+	// TC-03 : After login with Admin, verify all labels shown on Dashboard 	
+	@Test(priority = 3)
+	public void VerifyDashboardPageLabelsWithAdminUserLogin() throws InterruptedException
+	{
+		Thread.sleep(5000);
+		DashboardPage dpage = new DashboardPage(driver);
+		dpage.VerifyTotalUsersLabel();
+		logger.info("'Total Users' count is exist/displayed on Dashboard Page");
+		
+		dpage.VerifyActiveUsersLabel();
+		logger.info("'Active Users' count is exist/displayed on Dashboard Page");
+		
+		dpage.VerifyInactiveUsersLabel();
+		logger.info("'Inactive Users' count is exist/displayed on Dashboard Page");
+		
+		dpage.VerifyInfluencerUsersLabel();
+		logger.info("'Influencer Users' count is exist/displayed on Dashboard Page");
+		
+		
+		dpage.VerifyBrandUsersLabel();
+		logger.info("'Brand Users' count is exist/displayed on Dashboard Page");
+		
+		dpage.VerifyAgenciesUsersLabel();
+		logger.info("'Agencies Users' count is exist/displayed on Dashboard Page");
+		
+		dpage.VerifyInfluencerCampaignLabel();
+		logger.info("'Influencer Campaign' count is exist/displayed on Dashboard Page");
+		
+		dpage.VerifyPendingLabel();
+		logger.info("'Pending' count is exist/displayed on Dashboard Page under Influencer campaign section");
+		
+		dpage.VerifyRejectedLabel();
+		logger.info("'Reject' count is exist/displayed on Dashboard Page under Influencer campaign section");
+		
+		dpage.VerifyInfluencerCampaignActiveLabel();
+		logger.info("'Active' label is exist/displayed on Dashboard page under Influencer Campaign section");
+		
+		dpage.VerifyInfluencerCampaignCompletedLabel();
+		logger.info("'Completed' label is exist/displayed on Dashboard page under Influencer Campaign section");
+		
+		dpage.VerifyUpcomingLabel();
+		logger.info("'Upcoming' label is exist/displayed on Dashboard page under Influencer campaign section");
+		
+		dpage.VerifyCampaignAnalysisLabel();
+		logger.info("'Campaign Analysis' chart label is exist/displayed on Dashboard page");
+		
+		dpage.VerifyDraftLabel();
+		logger.info("'Draft' label is exist/displayed on Dashboard page under campaign Analysis chart");
+		
+		dpage.VerifyRequestsLabel();
+		logger.info("'Requist' label is exist/displayed on Dashboard page under campaign Analysis chart");
+		
+		dpage.VerifyReviewLabel();
+		logger.info("'Review' label is exist/displayed on Dashboard page under campaign Analysis chart");
+		
+		dpage.VerifyCampaignAnalysisCompletedLabel();
+		logger.info("'Completed' label is exist/displayed on Dashboard page under campaign Analysis chart");
+		
+		dpage.VerifyCampaignAnalysisActiveLabel();
+		logger.info("'Active' label is exist/displayed on Dashboard page under campaign Analysis chart");
+		
+		dpage.VerifyInactiveLabel();
+		logger.info("'Inactive' label is exist/displayed on Dashboard page under campaign Analysis chart");
+		
+		dpage.VerifyInfluencerPlatformsAnalysisLabel();
+		logger.info("'Influencer Platform Analysis' chart label is exist/displayed on Dashboard page");
+		
+		dpage.VerifyYouTubeLabel();
+		logger.info("'YouTube' label is exist/displayed on Dashboard page under Influencer Platform Analysis chart");
+		
+		dpage.VerifyInstagramLabel();
+		logger.info("'Instagram' label is exist/displayed on Dashboard page under Influencer Platform Analysis chart");
+		
+		dpage.VerifyTikTokLabel();
+		logger.info("'TikTok' label is exist/displayed on Dashboard page under Influencer Platform Analysis chart");
+		
+		dpage.VerifySpotifyLabel();
+		logger.info("'Spotify' label is exist/displayed on Dashboard page under Influencer Platform Analysis chart");
+	}
 }
--- src/test/java/com/ikkon/pageObjects/DashboardPage.java
+++ src/test/java/com/ikkon/pageObjects/DashboardPage.java
@@ -1,11 +1,13 @@
 package com.ikkon.pageObjects;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class DashboardPage {
 	// Create local object of WebDriver
@@ -16,6 +18,32 @@ public class DashboardPage {
 			ldriver = rdriver;
 			PageFactory.initElements(rdriver, this);
 		}
+	// Locators
+		@FindBy(xpath = "//h6[text()='Total Users']") public WebElement TotalUsers;
+		@FindBy(xpath = "//h6[text()='Active Users']") public WebElement ActiveUsers;
+		@FindBy(xpath = "//h6[text()='Inactive Users']") public WebElement InactiveUsers;
+		@FindBy(xpath = "//h6[text()='Influencer Users']") public WebElement InfluencerUsers;
+		@FindBy(xpath = "//h6[text()='Brand Users']") public WebElement BrandUsers;
+		@FindBy(xpath = "//h6[text()='Agencies Users']") public WebElement AgenciesUsers;
+		@FindBy(xpath = "//h5[text()='Influencer Campaign']") public WebElement InfluencerCampaign;
+		@FindBy(xpath = "//p[text()='Pending']") public WebElement Pending;
+		@FindBy(xpath = "//p[text()='Rejected']") public WebElement Rejected;
+		@FindBy(xpath = "//p[text()='Active'][@class='MuiTypography-root MuiTypography-body1 css-1990hm5']") public WebElement InfluencerCampaignActive;
+		@FindBy(xpath = "//div[@class = 'MuiBox-root css-6gplk5']//p[text()='Completed']") public WebElement InfluencerCampaignCompleted;
+		@FindBy(xpath = "//p[text()='Upcoming']") public WebElement Upcoming;
+		@FindBy(xpath = "//h5[text()='Campaign Analysis'] ") public WebElement CampaignAnalysis;
+		@FindBy(xpath = "//p[text()='Draft']") public WebElement Draft;
+		@FindBy(xpath = "//p[text()='Requests']") public WebElement Requests;
+		@FindBy(xpath = "//p[text()='Review']") public WebElement Review;
+		@FindBy(xpath = "//p[@class='MuiTypography-root MuiTypography-body1 css-1op1nui'][text()='Completed']") public WebElement CampaignAnalysisCompleted;
+		@FindBy(xpath = "//p[text()='Active'][@class='MuiTypography-root MuiTypography-body1 css-1op1nui']") public WebElement CampaignAnalysisActive;
+		@FindBy(xpath = "//p[text()='Inactive']") public WebElement Inactive;
+		@FindBy(xpath = "//h5[text()='Influencer Platforms Analysis']") public WebElement InfluencerPlatformsAnalysis;
+		@FindBy(xpath = "//p[text()='YouTube']") public WebElement YouTube;
+		@FindBy(xpath = "//p[text()='Instagram']") public WebElement Instagram;
+		@FindBy(xpath = "//p[text()='TikTok']") public WebElement TikTok;
+		@FindBy(xpath = "//p[text()='Spotify']") public WebElement Spotify;
+		
 		
 		
 		public void VerifyDashboardPageURL()
@@ -24,5 +52,129 @@ public class DashboardPage {
 			String expectedUrl = "https://ikkon.ae/dashboard";
 			assertEquals(actualUrl,expectedUrl);
 		}
+	
+		/*
+		 * public boolean VerifyTotalUsersLabel() { return TotalUsers.isDisplayed(); }
+		 */
+		
+		public void VerifyTotalUsersLabel()
+		{
+			Assert.assertTrue(TotalUsers.isDisplayed(),"Total Users label is not displayed");
+		}
+		
+		public boolean VerifyActiveUsersLabel()
+		{
+			return ActiveUsers.isDisplayed();
+		}
+		
+		public boolean VerifyInactiveUsersLabel()
+		{
+			return InactiveUsers.isDisplayed();
+		}
+		
+		public boolean VerifyInfluencerUsersLabel()
+		{
+			return InfluencerUsers.isDisplayed();
+		}
+		
+		public boolean VerifyBrandUsersLabel()
+		{
+			return BrandUsers.isDisplayed();
+		}
+		
+		public boolean VerifyAgenciesUsersLabel()
+		{
+			return AgenciesUsers.isDisplayed();
+		}
+		public boolean VerifyInfluencerCampaignLabel()
+		{
+			return InfluencerCampaign.isDisplayed();
+		}
+		
+		public boolean VerifyPendingLabel()
+		{
+			return Pending.isDisplayed();
+		}
+		
+		public boolean VerifyRejectedLabel()
+		{
+			return Rejected.isDisplayed();
+		}
+		
+		public boolean VerifyInfluencerCampaignActiveLabel()
+		{
+			return InfluencerCampaignActive.isDisplayed();
+		}
+		
+		public boolean VerifyInfluencerCampaignCompletedLabel()
+		{
+			return InfluencerCampaignCompleted.isDisplayed();
+		}
+		
+		public boolean VerifyUpcomingLabel()
+		{
+			return Upcoming.isDisplayed();
+		}
+		
+		public boolean VerifyCampaignAnalysisLabel()
+		{
+			return CampaignAnalysis.isDisplayed();
+		}
+		
+		public boolean VerifyDraftLabel()
+		{
+			return Draft.isDisplayed();
+		}
+		
+		public boolean VerifyRequestsLabel()
+		{
+			return Requests.isDisplayed();
+		}
+		
+		public boolean VerifyReviewLabel()
+		{
+			return Review.isDisplayed();
+		}
+		
+		public boolean VerifyCampaignAnalysisCompletedLabel()
+		{
+			return CampaignAnalysisCompleted.isDisplayed();
+		}
+		
+		public boolean VerifyCampaignAnalysisActiveLabel()
+		{
+			return CampaignAnalysisActive.isDisplayed();
+		}
+		
+		public boolean VerifyInactiveLabel()
+		{
+			return Inactive.isDisplayed();
+		}
+		
+		public boolean VerifyInfluencerPlatformsAnalysisLabel()
+		{
+			return InfluencerPlatformsAnalysis.isDisplayed();
+		}
+		
+		public boolean VerifyYouTubeLabel()
+		{
+			return YouTube.isDisplayed();			
+		}
+				
+		public boolean VerifyInstagramLabel()
+		{
+			return Instagram.isDisplayed();
+		}
+		
+		public boolean VerifyTikTokLabel()
+		{
+			return TikTok.isDisplayed();
+		}
+		
+		public boolean VerifySpotifyLabel()
+		{
+			return Spotify.isDisplayed();
+		}
+		
 		
 }
